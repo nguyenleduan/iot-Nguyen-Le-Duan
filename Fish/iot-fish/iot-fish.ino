85a1edb4b9fb78f83b4be703c5466357d7ded619
unknown <nguyenleduan.index@gmail.com>
2022-05-11 21:28:14 +0700

    nut bam

--- Fish/iot-fish/iot-fish.ino
+++ Fish/iot-fish/iot-fish.ino
@@ -1,3 +1,5 @@
+#include <OneButton.h>
+
 #include <DS3231.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
@@ -5,11 +7,12 @@
 const int minuteOnOff =2;
 const int SA1trig = 11; // Chan 8 trig cua SA1
 const int SA1echo = 11; // Chan 7 echo cua SA1
-const int SA2trig= 7; // Chan 13 trig cua SA1
-const int SA2echo= 8;  // Chan 12 echo cua SA2
+const int SA2trig= 13; // Chan 13 trig cua SA1
+const int SA2echo= 12;  // Chan 12 echo cua SA2
 
-const int relayIn= 3;  // Chan relayIn 3
-const int relayOut= 2;  // Chan relayOut 2 
+const int relayIn= 2;  // Chan relayIn 3
+const int relayOut= 3;  // Chan relayOut 2 
+const int buttonClick = 4;  // Chan relayOut 2 
 
 const int valueMaxBox= 5;  // Khoang cach khi nuoc day box CM
 const int valueMinBox= 20;  // Khoang cach khi nuoc day box CM
@@ -17,17 +20,23 @@ const int valueMinBox= 20;  // Khoang cach khi nuoc day box CM
 LiquidCrystal_I2C lcd(0x27,16,2);  // khai bao lcd
 DS3231 rtc(SDA,SCL); // thoi gian thuc 
 Time time;    
+OneButton buuton (4,true); 
+
+
+
+// <-------------------->
+int giatriTest;
 void setup() {
   Serial.begin(9600);//Mở cổng Serial ở 9600
   lcd.init();
   lcd.backlight(); 
-  
+  buuton.attachClick(thayNuoc);
   // pinMode(SA1trig, OUTPUT); //Chân trig xuất tín hiệu
   // pinMode(SA1echo, INPUT); 
   pinMode(SA2trig, OUTPUT); //Chân trig xuất tín hiệu
   pinMode(SA2echo, INPUT);    
   pinMode(relayIn, OUTPUT);     
-  pinMode(relayOut, OUTPUT);   
+  pinMode(relayOut, OUTPUT);    
   digitalWrite(relayIn,0); // off relayIN 
   digitalWrite(relayOut,0); // on relayOut  
 }
@@ -43,17 +52,41 @@ void loop() {
   // Serial.print("Khoảng cách: ");
   // Serial.print(khoangcach);
   // Serial.println("cm");
-thayNuoc();
+// thayNuoc();
 
   
+  buuton.tick(); //Kiểm tra trạng thái nút nhấn 
+//  Serial.print("Đếm xxx: "); 
+//       lcd.setCursor(0, 0); // set vi tri show
+//       lcd.print(" click :");  // noi dung     
+//       delay(5000); 
+
+//       lcd.clear();  
+  delay(10);     
+
+}
+
+void onclick(){
+ Serial.print("Đếm 1: "); 
+  int i =0;
+  
+  while(i < 5){ 
+      lcd.clear(); 
       lcd.setCursor(0, 0); // set vi tri show
       lcd.print(" DONE :");  // noi dung    
+      lcd.setCursor(6, 0); // set vi tri show
+      lcd.print(i);  // noi dung    
       lcd.setCursor(5, 1); // set vi tri show
       lcd.print(getKhoangCachSA(SA2trig,SA2echo));  // noi dung    
-      delay(5000); 
-      lcd.clear(); 
-
-
+      delay(1000);  
+      i++;
+      lcd.clear();  
+  }
+      // lcd.setCursor(0, 0); // set vi tri show
+      // lcd.print(" DONE :");  // noi dung    
+      // lcd.setCursor(5, 1); // set vi tri show
+      // lcd.print(getKhoangCachSA(SA2trig,SA2echo));  // noi dung    
+      // delay(10000); 
 }
 void setupSA(){
   pinMode(SA1trig, OUTPUT); //Chân trig xuất tín hiệu
